88b28597f10793b7400699dc7e22ce32c61cd78a
Martin Croker <mcroker@gmail.com>
2021-03-13 11:03:00 +0000

    fix(build): correct format of release.config.json

--- release.config.js
+++ release.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   branches: ['master', { name: 'beta', prerelease: true }],
-  plugins: ["@semantic-release/npm", {
-    "pkgRoot": "dist/ngx-firebase"
-  }]
+  plugins: [
+    ["@semantic-release/npm", {
+      "pkgRoot": "./dist/ngx-firebase"
+    }]
+  ]
 };
