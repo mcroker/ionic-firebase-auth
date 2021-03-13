71ac53ae2b0d93f5598f3dd9e36240c768b9120a
Martin Croker <mcroker@gmail.com>
2021-03-13 09:54:12 +0000

    fix(build): resolve pkgRoot

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: ['master', { name: 'beta', prerelease: true }],
   plugins: ["@semantic-release/npm", {
-    "pkgRoot": "dist/npx-firebase"
+    "pkgRoot": "dist/ngx-firebase"
   }]
 };
