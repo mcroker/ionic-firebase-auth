c107c5fbcfef757e519fb14d1ddd3e5a9b12ffca
Martin Croker <mcroker@gmail.com>
2021-03-17 13:07:24 +0000

    build: move sonar-scanner into release.config to get version no

--- client/release.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  branches: ['master', { name: 'beta', prerelease: true }],
-  plugins: [
-    ["@semantic-release/npm", {
-      "pkgRoot": "./dist/ngx-firebase"
-    }]
-  ]
-};
---
+++ release.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  branches: ['master', { name: 'beta', prerelease: true }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    ["@semantic-release/npm", {
+      "pkgRoot": "./client/dist/ngx-firebase"
+    }],
+    ["@semantic-release/exec", {
+      "successCmd": "./sonar-scanner/sonar-scanner-4.6.0.2311-macosx/bin/sonar-scanner -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectVersion=${nextRelease.version} -Dsonar.branch.name=${options.branch}"
+    }]
+  ]
+};
